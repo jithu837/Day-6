291467f0e4f7ba215b368cd9cef7d4c961f54b84
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-07 16:22:20 +0530

    Add files via upload

---
+++ Break_Continue.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Break_Continue
+ {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Break Example");
+        for(int i=0;i<n;i++){
+         if(i==2) break;
+         System.out.println(i);
+        }
+        System.out.println("Continue Example");
+        for(int i=0;i<n;i++){
+         if(i==2) continue;
+         System.out.println(i);
+        }
+    }
+    
+}
